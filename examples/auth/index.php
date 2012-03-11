fcc77fe8cb9f0e403affad7cdabccfb16529a801
Nijiko Yonskai <me@nijikokun.com>
2012-03-11 18:00:35 -0300

    Added comments and redirect upon login.

--- examples/auth/index.php
+++ examples/auth/index.php
@@ -1,8 +1,11 @@
 <?php
 include '../lib/http.php';
 
+// Our super-duper-secret password.
+$password = '1234';
+
 http\get('/login', function() {
-    if(http::$session->authd)
+    if(http::$session->logged_in)
     	http::redirect('/');
 
     echo '<form method="post">';
@@ -11,16 +14,22 @@ http\get('/login', function() {
     echo '</form>';
 });
 
-http\post('/login', function() {
-	if(http::$post->password != '1234')
+// Post request for login page, upon form submission
+// the user will land here.
+http\post('/login', function() use ($password) {
+	// Check password against user input.
+	if(http::$post->password != $password)
 		http::redirect('/login');
 
-	http::$session->authd = true;
+	// We made it through the check, user is now authenticated.
+	http::$session->logged_in = true;
+	http::redirect('/');
 });
 
+// Index Request.
 http\get('/', function () {
-    if(!http::$session->authd)
-    	echo 'Please <a href="login/">login</a>!';
+    if(!http::$session->logged_in)
+    	echo 'Please <a href="login/">login</a> to see a great message!';
     else
-    	echo 'Hello there good sir!';
+    	echo 'Hello there good sir, glad to see you logged in!';
 });
