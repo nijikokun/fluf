ca283317978df60f3f8782b8e01dcdd322619d2c
Nijiko Yonskai <nijikokun@gmail.com>
2012-12-09 15:16:30 -0600

    Add better callback validation for JSONP

--- lib/fluf.php
+++ lib/fluf.php
@@ -102,12 +102,21 @@ class fluf {
    * @param  Mixed  $result JSON/JSONP Response will be array based, all others are string based.
    */
   static function handleResult ($result) {
+    function is_valid_callback($subject) {
+      $identifier_syntax = '/^[$_\p{L}][$_\p{L}\p{Mn}\p{Mc}\p{Nd}\p{Pc}\x{200C}\x{200D}]*+$/u';
+      $reserved_words = array('break', 'do', 'instanceof', 'typeof', 'case', 'else', 'new', 'var', 'catch', 'finally', 
+        'return', 'void', 'continue', 'for', 'switch', 'while', 'debugger', 'function', 'this', 'with', 'default', 'if', 
+        'throw', 'delete', 'in', 'try', 'class', 'enum', 'extends', 'super', 'const', 'export', 'import', 'implements', 
+        'let', 'private', 'public', 'yield', 'interface', 'package', 'protected', 'static', 'null', 'true', 'false');
+      return preg_match($identifier_syntax, $subject) && ! in_array(mb_strtolower($subject, 'UTF-8'), $reserved_words);
+    }
+
     if (is_array($result)) { 
       $response = json_encode($result);
       self::set('header', 'Content-type', 'application/json; charset=utf-8');
       if (self::get('cors')) self::set('header', 'access-control-allow-origin', '*');
       if (self::get('jsonp') && self::$get->callback)
-        if (preg_match('/^([a-zA-Z0-9_]+)$', self::$get->callback) > 0)
+        if (is_valid_callback(self::$get->callback))
           $response = self::$get->callback . "($response)";
     } else $response = $result;
 
